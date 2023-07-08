b726c473cf444794141195370a8f871c7af71dfb
ruffyp <128737767+raphaelparr@users.noreply.github.com>
2023-07-08 18:49:10 +0200

    test

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -1,7 +1,45 @@
 export default function Home() {
+  const data = [
+    {
+      standardDeviation: 1,
+      ticker: "AAPL",
+      name: "Apple",
+      change: 12,
+      link: "link",
+    },
+    {
+      standardDeviation: 1.5,
+      ticker: "AMZN",
+      name: "Amazon",
+      change: 4,
+      link: "link",
+    },
+    {
+      standardDeviation: 1.2,
+      ticker: "META",
+      name: "Meta",
+      change: -10,
+      link: "link",
+    },
+    {
+      standardDeviation: 2,
+      ticker: "GOOG",
+      name: "Alphabet",
+      change: -3,
+      link: "link",
+    },
+    {
+      standardDeviation: 2.3,
+      ticker: "TSLA",
+      name: "Tesla",
+      change: 6,
+      link: "link",
+    },
+  ];
+
   return (
     <main className="flex flex-col items-center justify-between min-h-screen p-24 bg-mauve-3 text-indigo-12">
       incoming...
     </main>
-  )
+  );
 }
