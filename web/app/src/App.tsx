b82641244c0a1ff3bd64235d1a02c069c0beec35
ruffyp <128737767+raphaelparr@users.noreply.github.com>
2023-07-11 16:08:01 +0200

    dfgdfg

--- web/app/src/App.tsx
+++ web/app/src/App.tsx
@@ -1,27 +1,47 @@
+"use client";
+
 import { useState } from "react";
 import "./App.css";
 import { BsFillCalendarFill } from "react-icons/bs";
 import MainBox from "./components/MainBox/MainBox";
-import "./App.css";
+import { useSpring, animated } from "@react-spring/web";
 
 function App() {
+  const springs = useSpring({
+    from: { x: 0 },
+    to: { x: 100 },
+  });
+
   return (
     <>
-      <div className="FrontPage">
-        <div className="BigP">Lorem ipsum dolor sit amet.</div>
-        <div className="SmallP">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
-          assumenda quos maiores. Nulla neque ducimus molestiae quaerat labore,
-          iure cumque!
-        </div>
-        <div className="bg-gradient-to-t Background">
-          <div className="max-w-5xl Flexbox">
-            <MainBox>Today</MainBox>
-            <MainBox>Yesterday</MainBox>
-            <MainBox>Trailing Week</MainBox>
-            <MainBox>Trailing Month</MainBox>
+      <div className="Back">
+        <div className="FrontPage">
+          <div className="BigP">Lorem ipsum dolor sit amet.</div>
+          <div className="SmallP">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam
+            assumenda quos maiores. Nulla neque ducimus molestiae quaerat
+            labore, iure cumque!
+          </div>
+          <div className="bg-gradient-to-t Background">
+            <div className="max-w-5xl Flexbox">
+              <div>
+                <MainBox>Today</MainBox>
+              </div>
+              <MainBox>Yesterday</MainBox>
+              <MainBox>Trailing Week</MainBox>
+              <MainBox>Trailing Month</MainBox>
+            </div>
           </div>
         </div>
+        <animated.div
+          style={{
+            width: 80,
+            height: 80,
+            background: "#ff6d6d",
+            borderRadius: 8,
+            ...springs,
+          }}
+        />
       </div>
     </>
   );
