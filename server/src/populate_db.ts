fdf79e8c0d7789626324c55d39ad7a88e49d9097
Clueed <clueed@proton.me>
2023-07-14 17:44:03 +0200

    feat: adding sigma staleness routine

--- server/src/populate_db.ts
+++ server/src/populate_db.ts
@@ -3,7 +3,7 @@ import "dotenv/config";
 import { PolygonApi } from "./polygonApi/polygonApi.js";
 import { reverseIncrementDailyUpdate } from "./reverseIncrementDailyUpdate.js";
 import { supplementTickerDetails } from "./supplementTickerDetails.js";
-import { calcSigmas } from "./calcSigmas.js";
+import { dailySigmaRoutine } from "./dailySigmaRoutine.js";
 
 export const prisma = new PrismaClient();
 export const polygon = new PolygonApi();
@@ -12,9 +12,9 @@ async function main() {
   //await reverseIncrementDailyUpdate(polygon, prisma, true);
   //await supplementTickerDetails(prisma, polygon);
 
-  // TODO: calculations
-
-  await calcSigmas(prisma);
+  console.group("Checking sigma staleness...");
+  await dailySigmaRoutine();
+  console.groupEnd();
 }
 
 main()
