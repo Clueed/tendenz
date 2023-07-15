4cec1ee2e58a4e2a093abdf2c83ef13a9ea9aca7
Clueed <clueed@proton.me>
2023-07-15 18:44:20 +0200

    feat: reverseIncrementDailyUpdate rework

--- server/src/dailyRoutine/reverseIncrementDailyUpdate.ts
+++ server/src/dailyRoutine/reverseIncrementDailyUpdate.ts
@@ -1,67 +1,71 @@
-import { PrismaClient } from "@prisma/client";
-import { PolygonApi } from "../polygonApi/polygonApi.js";
 import { updateDaily } from "./updateDaily.js";
 import { formatDateString } from "../misc.js";
 import { aggregatesGroupedDaily } from "../polygonApi/aggregatesGroupedDaily.js";
 
-interface UpdateCounter {
-  [key: string]: number;
+interface UpdateStatus {
+  updated: number;
+  queryCount: number;
+  date: string;
 }
 
 export async function reverseIncrementDailyUpdate(
-  endOnNoUpdates: boolean = true,
-  startingDate: string | Date = new Date()
-): Promise<UpdateCounter> {
-  let startingDateObj: Date;
-
-  if (typeof startingDate === "string") {
-    startingDateObj = new Date(Date.parse(startingDate));
-  } else {
-    startingDateObj = startingDate;
-  }
-
-  let errorCounter: number = 0;
+  endOnNoUpdates: boolean = true
+): Promise<UpdateStatus[]> {
+  let stats: UpdateStatus[] = [];
 
   let daysPast: number = 0;
-
-  let updateCounter: UpdateCounter = {};
+  const startingDate = new Date();
 
   while (true) {
-    const targetDate =
-      startingDateObj.getTime() - daysPast * 24 * 60 * 60 * 1000;
+    const targetDate = startingDate.getTime() - daysPast * 24 * 60 * 60 * 1000;
     const dateString = formatDateString(targetDate);
     console.group(`Updating ${dateString}`);
 
     const results = await aggregatesGroupedDaily(dateString);
 
-    if (results === false) {
-      errorCounter++;
-
-      if (errorCounter === 5) {
-        daysPast++;
-      }
-
-      if (errorCounter >= 10) {
-        break;
-      }
-
-      continue;
-    }
+    let updateCount: number = 0;
 
     if (results.length > 0) {
-      updateCounter[dateString] = await updateDaily(results);
-
-      if (endOnNoUpdates && updateCounter[dateString] === 0) {
-        console.info("Ending update cycle. No new data available.");
-        break;
-      }
+      updateCount = await updateDaily(results);
     } else {
-      console.info(`No data available.`);
+      console.info("Not data available.");
+      updateCount = 0;
     }
+
+    stats.push({
+      updated: updateCount,
+      queryCount: results.length,
+      date: dateString,
+    });
 
     console.groupEnd();
     daysPast++;
+
+    const queryCounts = stats.reverse().map((day) => day.queryCount);
+    if (consecutiveZeros(queryCounts) > 6) {
+      // API error or more than 2 years back
+      break;
+    }
+
+    const updatedDays = stats.reverse().map((day) => day.updated);
+    if (endOnNoUpdates && consecutiveZeros(updatedDays) >= 5) {
+      break;
+    }
   }
 
-  return updateCounter;
+  return stats;
+}
+
+function consecutiveZeros(array: number[]) {
+  let zeros = 0;
+
+  for (const i of array) {
+    if (i === 0) {
+      zeros++;
+    } else {
+      break;
+    }
+  }
+
+  return zeros;
 }
