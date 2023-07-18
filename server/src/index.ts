4222feb48aeaf9b3a3b8c3f56b0ad1477b016efb
Clueed <clueed@proton.me>
2023-07-18 18:12:42 +0200

    feat: use prisma fastify plugin

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,11 +1,8 @@
 import Bree from "bree";
 import Fastify from "fastify";
-import { dailySigmaRoutine } from "./dailyRoutine/dailySigmaRoutine.js";
-import { reverseIncrementDailyUpdate } from "./dailyRoutine/reverseIncrementDailyUpdate.js";
-import { supplementTickerDetails } from "./dailyRoutine/supplementTickerDetails.js";
-import { prisma } from "./globals.js";
 import path from "node:path";
 import { fileURLToPath } from "node:url";
+import prismaPlugin from "./plugins/prisma.js";
 
 if (process.env.NODE_ENV === "production") {
   console.debug = function () {};
@@ -15,8 +12,10 @@ const fastify = Fastify({
   logger: true,
 });
 
+fastify.register(prismaPlugin);
+
 fastify.get("/", async (request, reply) => {
-  const test = await prisma.sigmaUsStocksYesterday.findMany({
+  const test = await fastify.prisma.sigmaUsStocksYesterday.findMany({
     orderBy: {
       weight: "desc",
     },
@@ -46,12 +45,11 @@ const bree = new Bree({
 });
 
 const start = async () => {
+  bree.start();
   try {
-    bree.start();
     await fastify.listen({ port: 3000, host: "0.0.0.0" });
   } catch (err) {
     fastify.log.error(err);
-    await prisma.$disconnect();
     process.exit(1);
   }
 };
