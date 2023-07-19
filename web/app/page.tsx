26336e97bef56e8bcca75b3da7c4f41c1d1a3a3b
Clueed <clueed@proton.me>
2023-07-19 19:36:04 +0200

    web/feat: handle api error

--- web/app/page.tsx
+++ web/app/page.tsx
@@ -13,17 +13,15 @@ export interface SigmaUsStocksYesterday {
 }
 
 async function getData() {
-  const res = await fetch("https://tendenz-server.fly.dev/");
+  try {
+    const res = await fetch("https://tendenz-server.fly.dev/");
+    return res.json() as Promise<SigmaUsStocksYesterday[]>;
+  } catch (e) {
+    console.error(e);
+    return [];
+  }
   // The return value is *not* serialized
   // You can return Date, Map, Set, etc.
-
-  // Recommendation: handle errors
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
-  }
-
-  return res.json() as Promise<SigmaUsStocksYesterday[]>;
 }
 
 export default async function Home() {
@@ -32,13 +30,11 @@ export default async function Home() {
     <main className="flex flex-col items-center">
       <Hero />
       <section className="max-w-[90vw]">
-        <h1 className="mx-10 mt-12 text-4xl font-normal text-indigo-11">
+        <h1 className="mt-12 text-4xl font-normal text-indigo-11">
           Yesterday&apos;s anomalies
         </h1>
         <section className="mt-6">
-          <h2 className="mx-10 text-3xl font-normal text-slate-12">
-            US stocks
-          </h2>
+          <h2 className="text-3xl font-normal text-slate-12">US stocks</h2>
           <div className="mt-[4vh] flex justify-center">
             <MainBox data={data} />
           </div>
@@ -46,14 +42,19 @@ export default async function Home() {
         <section className="mt-[10rem]">
           <h2 className="text-3xl font-normal text-slate-12">Coming soon...</h2>
           <div className="flex flex-wrap justify-start gap-10 mx-10 my-10 text-2xl bg-clip-text text-black-a1 bg-gradient-to-br from-indigo-10 to-sky-10 via-violet-10">
-            <div>global equities</div>
             <div>options</div>
-            <div>currencies</div>
+            <div>global equities</div>
+            <div>commodities</div>
+            <div>credit default swaps</div>
+            <div>foreign exchange</div>
             <div>equity indexes</div>
             <div>cryptocurrencies</div>
             <div>bonds</div>
-            <div>economic indicators</div>
+            <div>derivatives</div>
+            <div>notes</div>
             <div>interest rates</div>
+            <div>mortgage-backed securities</div>
+            <div>economic indicators</div>
           </div>
         </section>
       </section>
