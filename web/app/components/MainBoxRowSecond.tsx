71ea9f0938c38dcbb6f15872c612d9d9b8c7ee75
Clueed <clueed@proton.me>
2023-07-21 15:46:34 +0200

    web/fix: rowSecond props

--- web/app/components/MainBoxRowSecond.tsx
+++ web/app/components/MainBoxRowSecond.tsx
@@ -1,18 +1,26 @@
 "use client";
-export function MainBoxRowSecond(props) {
+export function MainBoxRowSecond({
+  dailyReturn,
+  secondLastClose,
+  lastClose,
+}: {
+  dailyReturn: string;
+  secondLastClose: string;
+  lastClose: string;
+}) {
   return (
     <div className="flex justify-around gap-2 text-slate-12">
       <div className="flex flex-col p-2 text-right">
-        <div className="text-xl">{props.secondLastClose}</div>
+        <div className="text-xl">{secondLastClose}</div>
         <div className="text-xs leading-tight">day before yesterday</div>
         <div className="text-xs leading-tight text-slate-11">close price</div>
       </div>
       <div className="flex flex-col p-2 text-right">
-        <div className="text-xl">{props.dailyReturn}</div>
+        <div className="text-xl">{dailyReturn}</div>
         <div className="text-xs leading-tight">return</div>
       </div>
       <div className="flex flex-col p-2 text-right">
-        <div className="text-xl">{props.lastClose}</div>
+        <div className="text-xl">{lastClose}</div>
         <div className="text-xs leading-tight">yesterday</div>
         <div className="text-xs leading-tight text-slate-11">close price</div>
       </div>
