5a5c75e420d8119d62568ce3c0ca87320ab8c9ff
Clueed <clueed@proton.me>
2023-07-25 10:12:24 +0200

    web/feat: make pop generic and stylish

--- web/app/components/Timer.tsx
+++ web/app/components/Timer.tsx
@@ -1,6 +1,7 @@
 "use client";
 import React from "react";
 import Pop from "./Pop";
+import classNames from "classnames";
 
 type Props = {};
 
@@ -9,24 +10,37 @@ export default function Timer({}: Props) {
 
   return (
     <Pop
-      maxWidth="max-w-xs"
-      popover={`${17} hours until next update. Data is updated early in the morning after trading days.`}
+      blur="2xl"
+      popoverContent={
+        <div className="w-40 text-slate-12">
+          {timeTill} hours until the next update. Data is available after
+          trading days with a one day delay.
+        </div>
+      }
+      popoverColor="slate"
     >
-      <div className="flex items-center gap-1.5 px-3 py-1 stroke-2 rounded-xl bg-slate-6 text-slate-11">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          height="1em"
-          viewBox="0 0 512 512"
-          fill="currentColor"
+      {(open) => (
+        <div
+          className={classNames(
+            "flex items-center gap-1.5 px-3 py-1 stroke-2 rounded-xl bg-slate-6 text-slate-11",
+            { "text-slate-12 shadow-2xl drop-shadow-xl": open }
+          )}
         >
-          Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com
-          License - https://fontawesome.com/license (Commercial License)
-          Copyright 2023 Fonticons, Inc.
-          <path d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z" />
-        </svg>
-        <div className="leading-none"></div>
-        {timeTill}h
-      </div>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="1em"
+            viewBox="0 0 512 512"
+            fill="currentColor"
+          >
+            Font Awesome Free 6.4.0 by @fontawesome - https://fontawesome.com
+            License - https://fontawesome.com/license (Commercial License)
+            Copyright 2023 Fonticons, Inc.
+            <path d="M464 256A208 208 0 1 1 48 256a208 208 0 1 1 416 0zM0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM232 120V256c0 8 4 15.5 10.7 20l96 64c11 7.4 25.9 4.4 33.3-6.7s4.4-25.9-6.7-33.3L280 243.2V120c0-13.3-10.7-24-24-24s-24 10.7-24 24z" />
+          </svg>
+          <div className="leading-none"></div>
+          {timeTill}h
+        </div>
+      )}
     </Pop>
   );
 }
