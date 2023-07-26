20f8415ccbe8819b2b2f62109d057b08b03474a1
Clueed <clueed@proton.me>
2023-07-27 00:04:10 +0200

    web/feat: blur nav

--- web/app/components/Navbar/NavigationBar.tsx
+++ web/app/components/Navbar/NavigationBar.tsx
@@ -7,7 +7,7 @@ export default function NavigationBar() {
     <nav className="sticky top-0 z-50 flex items-center justify-start w-full py-5 ">
       <Link
         href="/"
-        className="w-[8.1rem] text-slate-1 bg-slate-12 py-4 pl-6 pr-7 shadow-xl rounded-r-xl"
+        className="w-[8.1rem] text-slate-1 bg-slate-a12 backdrop-blur py-4 pl-6 pr-7 shadow-xl rounded-r-xl"
       >
         <Logo />
       </Link>
