622393df50cd6a8eefb1fdc3c1415f4c6a226079
Clueed <clueed@proton.me>
2023-07-26 20:24:30 +0200

    web/fix: hide laggy animation card list

--- web/app/components/Sigma/SigmaList.tsx
+++ web/app/components/Sigma/SigmaList.tsx
@@ -4,6 +4,7 @@ import { useState } from "react";
 import { tendenzApiSigmaYesterday } from "../../page";
 import SigmaCard from "./SigmaCard";
 import Timer from "../Timer";
+import { motion } from "framer-motion";
 
 export default function SigmaList({
   data,
@@ -28,29 +29,31 @@ export default function SigmaList({
         className="relative flex flex-col w-full gap-3"
         onValueChange={(o) => setOpen(o)}
       >
-        {data.length > 0
-          ? data.map((entry, i) => (
-              <SigmaCard
-                key={entry.ticker}
-                entry={entry}
-                expanded={expandedKey === entry.ticker}
-              />
-            ))
-          : [...Array(4).keys()].map((i) => (
-              <>
-                <div
-                  key={i}
-                  className="h-16 px-5 py-2 my-2 rounded-lg blur-sm w-100 bg-slate-5"
-                >
-                  {" "}
-                </div>
-                <div className="absolute flex items-center justify-center w-full h-full ">
-                  <div className="text-2xl font-light text-slate-10">
-                    Loading...
+        <motion.div layout="size" layoutRoot>
+          {data.length > 0
+            ? data.map((entry, i) => (
+                <SigmaCard
+                  key={entry.ticker}
+                  entry={entry}
+                  expanded={expandedKey === entry.ticker}
+                />
+              ))
+            : [...Array(4).keys()].map((i) => (
+                <>
+                  <div
+                    key={i}
+                    className="h-16 px-5 py-2 my-2 rounded-lg blur-sm w-100 bg-slate-5"
+                  >
+                    {" "}
                   </div>
-                </div>
-              </>
-            ))}
+                  <div className="absolute flex items-center justify-center w-full h-full ">
+                    <div className="text-2xl font-light text-slate-10">
+                      Loading...
+                    </div>
+                  </div>
+                </>
+              ))}
+        </motion.div>
       </Accordion.Root>
     </>
   );
