cc4e116e6ab26ead4302e393bccdc29791163039
Clueed <clueed@proton.me>
2023-07-27 13:38:41 +0200

    web/feat: deskop responsiveness 1

--- web/app/components/Sigma/SigmaCard.tsx
+++ web/app/components/Sigma/SigmaCard.tsx
@@ -5,7 +5,6 @@ import * as Accordion from "@radix-ui/react-accordion";
 import { AnimatePresence, motion } from "framer-motion";
 import { SigmaCardHeader } from "./SigmaCardHeader";
 import { SigmaCardBody } from "./SigmaCardBody";
-import Container from "../Container";
 
 export default function SigmaCard({
   entry,
@@ -17,10 +16,10 @@ export default function SigmaCard({
   return (
     <Accordion.Item
       value={entry.ticker}
-      className="relative grid grid-cols-default"
+      className="relative grid py-2 grid-cols-default"
     >
       <motion.div
-        className="absolute top-0 right-0 w-full h-full shadow-sm from-sky-a4 to-indigo-a5 bg-gradient-to-br -z-10 sm:rounded-xl col-span-full "
+        className="absolute top-0 right-0 w-full h-full shadow-sm from-sky-a4 to-indigo-a5 bg-gradient-to-br -z-10 sm:rounded-xl col-span-full sm:col-start-2 sm:col-end-2"
         initial={{ opacity: 0 }}
         animate={{ opacity: expanded ? 1 : 0 }}
         transition={{ duration: 0.5, ease: "easeInOut" }}
@@ -35,7 +34,9 @@ export default function SigmaCard({
             ticker={entry.ticker}
           />
         </Accordion.Trigger>
-        <AnimatePresence>
+      </div>
+      <div className="col-span-2 col-start-2 sm:col-span-1 sm:col-start-2">
+        <AnimatePresence presenceAffectsLayout>
           {expanded && (
             <Accordion.Content asChild forceMount>
               <motion.div
