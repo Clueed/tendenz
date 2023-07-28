3857b56f6a57c5f2013859067144b286a705322e
Clueed <clueed@proton.me>
2023-07-28 11:50:50 +0200

    web/fix: yahoo btn hover layout fix

--- web/app/components/Sigma/YahooButton.tsx
+++ web/app/components/Sigma/YahooButton.tsx
@@ -15,8 +15,8 @@ export function YahooButton({ ticker }: { ticker: string }) {
       whileHover={{
         paddingLeft: "0.75rem",
         paddingRight: "0.75rem",
-        marginLeft: "-0.25rem",
-        marginRight: "-0.25rem",
+        marginLeft: "-0.375rem",
+        marginRight: "-0.375rem",
         gap: "0.5rem",
         color: "var(--violet-12)",
       }}
@@ -24,6 +24,7 @@ export function YahooButton({ ticker }: { ticker: string }) {
       rel="noopener noreferrer"
       href={`https://finance.yahoo.com/quote/${ticker}`}
       className="flex items-center gap-1 py-2 shadow-sm rounded-l-md from-violet-a6 to-violet-a7 bg-gradient-to-br"
+      transition={{ type: "spring", duration: 0.75, bounce: 0.1 }}
     >
       <svg
         xmlns="http://www.w3.org/2000/svg"
