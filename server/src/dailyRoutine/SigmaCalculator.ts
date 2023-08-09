78181b7520b8f497a218515764c02d9a47baa924
Clueed <clueed@proton.me>
2023-08-09 15:52:29 +0200

    server/refactor: sigmaCalculator

---
+++ server/src/dailyRoutine/SigmaCalculator.ts
@@ -0,0 +1,82 @@
+import { Prisma } from '@prisma/client'
+import { AssertionError } from 'assert'
+import { DateTime } from 'luxon'
+import { DatabaseApi } from '../lib/databaseApi/databaseApi.js'
+import { SigmaMath } from './SigmaMath.js'
+
+export class SigmaCalculator {
+	constructor(private db: DatabaseApi) {}
+
+	async run() {
+		const mostRecentDate = await this.db.getMostRecentDate()
+		const dailys = await this.db.getTickersWithNameOnDate(mostRecentDate)
+
+		for (const { ticker, name } of dailys) {
+			let sigmaRow: Prisma.SigmaUsStocksYesterdayCreateInput
+			try {
+				sigmaRow = await this.constructSigmaRow(ticker, name)
+			} catch (e) {
+				if (e instanceof AssertionError) {
+					console.warn(e.message)
+					continue
+				} else {
+					console.error(e)
+					break
+				}
+			}
+			this.db.createSigmaYesterday(sigmaRow)
+		}
+	}
+
+	private async constructSigmaRow(
+		ticker: string,
+		name: string | undefined,
+	): Promise<Prisma.SigmaUsStocksYesterdayCreateInput> {
+		const now = DateTime.now()
+		const earliestDate = now.minus({ year: 2 })
+		const dailys = await this.db.getDailyInDateRange(
+			ticker,
+			now.toJSDate(),
+			earliestDate.toJSDate(),
+		)
+
+		const minPopulation = 30
+		if (dailys.length < minPopulation) {
+			throw new Error(
+				`${ticker} does not have enough data points (${dailys.length} < ${minPopulation}). Skipping...`,
+			)
+		}
+
+		const mostRecentMarketCap = dailys.find(daily => daily.marketCap)?.marketCap
+
+		const sortedDailys = SigmaMath.sortByDateDesc(dailys)
+		const LogReturns = SigmaMath.calcLogReturnsAsc(sortedDailys)
+
+		const sortedLogReturns = SigmaMath.sortByDateDesc(LogReturns)
+		const [lastLogReturn, secondLastLogReturn, ...logReturnPopulation] =
+			sortedLogReturns
+
+		const { sigma, n, stdev, mean } = SigmaMath.calcSigma(
+			logReturnPopulation.map(r => r.logReturn),
+			lastLogReturn.logReturn,
+		)
+
+		return {
+			ticker,
+			name,
+			sigma,
+			absSigma: Math.abs(sigma),
+			stdLogReturn: stdev,
+			meanLogReturn: mean,
+			sampleSize: n,
+			weight: 0,
+			lastLogReturn: lastLogReturn.logReturn,
+			lastClose: lastLogReturn.close,
+			lastDate: lastLogReturn.date,
+			secondLastLogReturn: secondLastLogReturn.logReturn,
+			secondLastClose: secondLastLogReturn.close,
+			secondLastDate: secondLastLogReturn.date,
+			marketCap: mostRecentMarketCap,
+		}
+	}
+}
---
+++ server/src/dailyRoutine/SigmaMath.ts
@@ -0,0 +1,60 @@
+interface getDailyStockReturn {
+	marketCap: number | null
+	close: number
+	date: Date
+}
+
+interface LogReturns extends getDailyStockReturn {
+	logReturn: number
+}
+
+export class SigmaMath {
+	static calcSigma(population: number[], sample: number) {
+		const n = population.length
+		const mean = this.calculateMean(population)
+		const stdev = this.calculateStandardDeviation(population, mean)
+		const lastSigma = (sample - mean) / stdev
+		return { sigma: lastSigma, stdev, mean, n }
+	}
+
+	private static calculateMean(data: number[]): number {
+		const sum = data.reduce((a, b) => a + b, 0)
+		return sum / data.length
+	}
+
+	private static calculateStandardDeviation(
+		data: number[],
+		mean: number,
+	): number {
+		const squaredDifferences = data.map(x => Math.pow(x - mean, 2))
+		const sumOfSquaredDifferences = squaredDifferences.reduce(
+			(a, b) => a + b,
+			0,
+		)
+		return Math.sqrt(sumOfSquaredDifferences / data.length)
+	}
+
+	static calcLogReturnsAsc(
+		sortedDataPoints: getDailyStockReturn[],
+	): LogReturns[] {
+		const dataPointsWithLogReturn: LogReturns[] = []
+
+		for (let i = 1; i < sortedDataPoints.length; i++) {
+			const curr = sortedDataPoints[i]
+			const prev = sortedDataPoints[i - 1]
+			const logReturn = Math.log(curr.close / prev.close)
+			dataPointsWithLogReturn.push({
+				...curr,
+				logReturn,
+			})
+		}
+
+		return dataPointsWithLogReturn
+	}
+
+	static sortByDateDesc<T extends getDailyStockReturn>(dataPoints: T[]): T[] {
+		return [...dataPoints].sort((a, b) => {
+			return a.date.getTime() - b.date.getTime()
+		})
+	}
+}
