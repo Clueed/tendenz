6e907bd39deeab356a4a6afd7954410061413609
Clueed <clueed@proton.me>
2023-08-11 17:19:38 +0200

    text(comingsoon)

--- web/app/components/ComingSoon.tsx
+++ web/app/components/ComingSoon.tsx
@@ -30,7 +30,7 @@ export function ComingSoon() {
 				<div className="mt-4 text-xl text-slate-11 sm:text-2xl">
 					<p>
 						<Balancer>
-							scale to more markets delivering only key movements at a glance.
+							explore more markets effortlessly, catching key trends at a glance
 						</Balancer>
 					</p>
 				</div>
@@ -47,7 +47,7 @@ export function ComingSoon() {
 				<div className="mt-4 text-xl text-slate-11 sm:text-2xl">
 					<p>
 						<Balancer>
-							spot changes over time by comparing with weekly and monthly data.
+							spot changes over time by comparing with weekly and monthly data
 						</Balancer>
 					</p>
 				</div>
