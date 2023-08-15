87c01f1e101620b7741f9fa5b5665e00fcb6da1b
Clueed <clueed@proton.me>
2023-08-15 10:45:18 +0200

    feat(marketCapCategory): find closest not of round

--- web/app/lib/getMarketCapCategory.tsx
+++ web/app/lib/getMarketCapCategory.tsx
@@ -1,24 +1,26 @@
+const UNITS = [
+	{ unit: 'trillion', value: 1e12 },
+	{ unit: 'billion', value: 1e9 },
+	{ unit: 'million', value: 1e6 },
+] as const
+
+const bucketValues = [
+	1,
+	5,
+	10,
+	...Array.from({ length: 5 }, (_, i) => (i + 1) * 20),
+	...Array.from({ length: 8 }, (_, i) => (i + 1) * 100 + 100),
+] as const
+
 export function getMarketCapCategory(marketCap: number) {
-	const UNITS = [
-		{ unit: 'trillion', value: 1e12 },
-		{ unit: 'billion', value: 1e9 },
-		{ unit: 'million', value: 1e6 },
-	]
-
-	const bucketValues = [
-		...Array.from({ length: 10 }, (_, i) => i + 1),
-		...Array.from({ length: 4 }, (_, i) => (i + 1) * 25),
-		...Array.from({ length: 8 }, (_, i) => (i + 1) * 100 + 100),
-	]
-
 	const { unit, value: unitValue } =
 		UNITS.find(({ value }) => marketCap >= value) || UNITS[2]
 
 	const rounded = Math.round(marketCap / unitValue)
 
-	const bucket = bucketValues
-		.reverse()
-		.find(bucketValue => rounded >= bucketValue) as number
+	const bucket = [...bucketValues].reverse().reduce((prev, curr) => {
+		return Math.abs(curr - rounded) < Math.abs(prev - rounded) ? curr : prev
+	})
 
 	const formatted = `$${bucket}${unit.charAt(0)}`
 
