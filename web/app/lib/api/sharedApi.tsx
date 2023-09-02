9817b789221ead217fe6bce9677f7de5fa7a3750
Clueed <clueed@proton.me>
2023-09-02 13:18:04 +0200

    web/chore: get api base url from env

--- web/app/lib/api/sharedApi.tsx
+++ web/app/lib/api/sharedApi.tsx
@@ -1,2 +1,4 @@
-export const getStocksURL = (page: number, minMarketCap: number) =>
-	`https://tendenz-server.fly.dev/us-stocks/daily/${page}?minMarketCap=${minMarketCap}`
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "https://tendenz-server.fly.dev"
+
+export const getStocksURL = (page: number, minMarketCap: number) => 
+	BASE_URL + `/us-stocks/daily/${page}?minMarketCap=${minMarketCap}`
