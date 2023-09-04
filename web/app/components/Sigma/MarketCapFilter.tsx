3002b3b32f6e8e9097a10f77890ff45d8ff77f65
Clueed <clueed@proton.me>
2023-09-04 14:55:42 +0200

    web/proto(marketCapSlider): with react-form-hook

--- web/app/components/Sigma/MarketCapFilter.tsx
+++ web/app/components/Sigma/MarketCapFilter.tsx
@@ -1,5 +1,24 @@
 import * as Slider from '@radix-ui/react-slider'
 import { Dispatch, SetStateAction, useState } from 'react'
+import { Controller, SubmitHandler, useForm } from 'react-hook-form'
+
+type Inputs = {
+	textfield: number | string
+}
+
+const valueMap: Record<number, number> = {
+	0: 0,
+	1: 1e6,
+	2: 10e6,
+	3: 100e6,
+	4: 500e6,
+	5: 1e9,
+	6: 10e9,
+	7: 100e9,
+	8: 500e9,
+	9: 1000e9,
+}
+
 export default function MarketCapFilter<T extends string[]>({
 	selectedKey,
 	selectKey,
@@ -9,101 +28,92 @@ export default function MarketCapFilter<T extends string[]>({
 	selectKey: Dispatch<SetStateAction<T[number]>>
 	allKeys: T
 }) {
+	const {
+		register,
+		handleSubmit,
+		watch,
+		formState: { errors },
+		control,
+		setValue,
+	} = useForm<Inputs>({
+		defaultValues: {
+			textfield: '1b',
+		},
+	})
+	const onSubmit: SubmitHandler<Inputs> = data => {
+		console.log(data)
+	}
 	const [minMarketCap, setMinMarketCap] = useState<number>(1e9)
-
-	const valueMap: Record<number, number> = {
-		0: 0,
-		1: 1e6,
-		2: 10e6,
-		3: 100e6,
-		4: 500e6,
-		5: 1e9,
-		6: 10e9,
-		7: 100e9,
-		8: 500e9,
-		9: 1000e9,
-	}
-
-	function handleSlider(value: number) {
-		setSliderValue(value)
-		console.log('value :>> ', value)
-
-		const newMinMarketCap = valueMap[value]
-		setMinMarketCap(newMinMarketCap)
-		setInputValue(convertNumberToString(newMinMarketCap))
-	}
-
-	function handleInput(input: string) {
-		setInputValue(input)
-
-		const number = convertNumber(input)
-		setMinMarketCap(number)
-
-		console.log('Object.entries(valueMap) :>> ', Object.entries(valueMap))
-		for (const i of Object.entries(valueMap)) {
-			console.log('i :>> ', i)
-			if (i[1] >= number) {
-				console.log('i[1] :>> ', i[1])
-				setSliderValue(Number(i[0]))
-				break
-			}
-		}
-	}
-
-	const [sliderValue, setSliderValue] = useState<number>(5)
-	const [inputValue, setInputValue] = useState<string>(
-		convertNumberToString(minMarketCap),
-	)
 
 	return (
 		<div>
-			<form>
+			<form onSubmit={handleSubmit(onSubmit)}>
 				${' '}
-				<input
-					value={inputValue}
-					className="bg-slate-a1 tracking-wide"
-					onChange={e => handleInput(e.target.value)}
+				<Controller
+					control={control}
+					name="textfield"
+					render={({ field }) => (
+						<>
+							<input
+								defaultValue={
+									typeof field.value === 'string'
+										? field.value
+										: convertNumberToString(field.value)
+								}
+								value={field.value === 0 ? '' : field.value}
+								onChange={e => field.onChange(e.target.value)}
+							/>
+							<Slider.Root
+								className="relative flex h-5 w-full touch-none select-none items-center"
+								max={9}
+								step={1}
+								onValueChange={v => {
+									const newValue = valueMap[Number(v[0])]
+
+									field.onChange(convertNumberToString(newValue))
+								}}
+								value={[
+									Number(
+										Object.entries(valueMap)
+											.reverse()
+											.find(
+												([index, value]) =>
+													value <= convertStringToNumber(field.value),
+											)?.[0],
+									),
+								]}
+							>
+								<Slider.Track className="relative h-[3px] grow rounded-full bg-slate-a5">
+									<Slider.Range className="absolute h-full rounded-full bg-slate-a10" />
+								</Slider.Track>
+								<Slider.Thumb
+									className="focus:shadow-blackA8 block h-4 w-4 rounded-[10px] bg-slate-11 hover:bg-violet-5 focus:shadow-[0_0_0_5px] focus:outline-none"
+									aria-label="Volume"
+								/>
+							</Slider.Root>
+						</>
+					)}
 				/>
-				<Slider.Root
-					className="relative flex h-5 w-full touch-none select-none items-center"
-					max={9}
-					step={1}
-					value={[sliderValue]}
-					onValueChange={v => handleSlider(v[0])}
-				>
-					<Slider.Track className="relative h-[3px] grow rounded-full bg-slate-a5">
-						<Slider.Range className="absolute h-full rounded-full bg-slate-a10" />
-					</Slider.Track>
-					<Slider.Thumb
-						className="focus:shadow-blackA8 block h-4 w-4 rounded-[10px] bg-slate-11 hover:bg-violet-5 focus:shadow-[0_0_0_5px] focus:outline-none"
-						aria-label="Volume"
-					/>
-				</Slider.Root>
 			</form>
 		</div>
 	)
 }
 
-function convertNumber(input: string) {
+const matchString = (input: string) => {
 	const numberPattern = /^(\d+(\.\d+)?)\s*([kKmMbBtT])?$/
 	const match = input.trim().match(numberPattern)
 
-	console.log('match :>> ', match)
-
 	if (!match) {
-		// Invalid input
-		return NaN
+		return { match: false }
 	}
 
-	const value = parseFloat(match[1])
-	const suffix = match[3]
+	return { match: true, value: parseFloat(match[1]), unit: match[3] }
+}
 
-	if (isNaN(value)) {
-		return NaN
-	}
-
-	if (suffix) {
-		switch (suffix.toLowerCase()) {
+function convertStringToNumber(input: string): number {
+	const { match, unit, value } = matchString(input)
+	if (unit && value) {
+		switch (unit.toLowerCase()) {
 			case 'k':
 				return value * 1e3
 			case 'm':
@@ -112,13 +122,11 @@ function convertNumber(input: string) {
 				return value * 1e9
 			case 't':
 				return value * 1e12
-			default:
-				// Invalid suffix
-				return NaN
 		}
+		return value
 	}
 
-	return value
+	return Number(input)
 }
 
 function convertNumberToString(value: number) {
