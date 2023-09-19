46384b1e6d948e90ccf46f20f721938bafc89c1f
Clueed <clueed@proton.me>
2023-09-20 00:06:08 +0200

    wip

--- web/app/components/Navbar/NavigationBar.tsx
+++ web/app/components/Navbar/NavigationBar.tsx
@@ -5,15 +5,15 @@ import { Logo } from './Logo'
 export default function NavigationBar() {
 	return (
 		<nav className="grid w-full grid-cols-default pt-[max(4vh_,3.5rem)]">
-			<div className="col-start-2 flex justify-between">
+			<div className="col-start-2 grid grid-cols-2 sm:grid-cols-3">
 				<Link
 					aria-label="homepage"
 					href="/"
-					className="w-32 text-slate1 opacity-90 transition-all hover:text-indigo12 sm:mx-auto sm:w-32"
+					className="w-32 text-slate1 opacity-90 transition-colors hover:text-indigo12 sm:col-start-2 sm:w-32 sm:place-self-center"
 				>
 					<Logo />
 				</Link>
-				<ColorThemeButton />
+				<ColorThemeButton className="place-self-end" />
 			</div>
 		</nav>
 	)
