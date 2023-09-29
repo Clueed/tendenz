8dd5ae2ce45b187fab4267bc51f6ce405b763ab1
Clueed <clueed@proton.me>
2023-09-29 10:40:42 +0200

    fix(filterCluster): h alignment

--- web/app/components/FilterCluster/FilterCluster.tsx
+++ web/app/components/FilterCluster/FilterCluster.tsx
@@ -4,7 +4,7 @@ import StockTypeToggle from './StockTypeToggle'
 export function FilterCluster({}) {
 	return (
 		<div className="mb-[2vh] mt-[1vh] grid grid-cols-default">
-			<div className="col-start-2 grid grid-cols-2 items-start gap-x-5">
+			<div className="col-start-2 grid grid-cols-2 items-end gap-x-5">
 				<StockTypeToggle />
 
 				<MarketCapFilter />
@@ -14,7 +14,7 @@ export function FilterCluster({}) {
 					</span>{' '}
 					<span className="text-slate11">United&nbsp;States</span>
 				</div>
-				<div className="flex items-start justify-end text-right">
+				<div className="text-right leading-none">
 					<MarketCapFilterLabel />
 				</div>
 			</div>
